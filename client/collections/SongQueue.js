bff21e441d5c5209073d10fcb421eafd5a8bc0eb
Elcin Erkin <elcinerkin@gmail.com>
2014-05-01 17:19:16 -0700

    events to be listened added

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -1,8 +1,24 @@
 // SongQueue.js - Defines a backbone model class for the song queue.
 var SongQueue = Songs.extend({
+// this is more of a model then a collection
+// this model will provide the queue the events to be listened
+// the song queue will broadcast when a song is clicked on the queue model
+  initialize: function() {
+  	this.on('add', function(){}, this);
 
-  initialize: function(){
+  	this.on('remove', function(){}, this);
+  	
+  	this.on('dequeue', function(){}, this);
+  	
+  	this.on('ended', function(){}, this);
+  },
+
+  enqueue: function() {
+  		this.trigger('enqueue', this);
+  },
+  
+  dequeue: function() {
+  		this.trigger('dequeue', this);
   }
-
-
+  
 });
